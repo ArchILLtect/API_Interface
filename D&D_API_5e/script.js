5556dd47709ac7094bfbb0a3f80ffa91a5afe4a8
ArchILLtect <nickhansonsr@gmail.com>
2023-10-14 14:43:47 -0500

    Add NavBar basics to D&D interface

--- D&D_API_5e/script.js
+++ D&D_API_5e/script.js
@@ -1,18 +1,68 @@
 /* Created on 10/11/23 by ArchILLtect for the Coursera program "Javascript for Beginners Specialization" - Course 2 - "Animation with JavaScript and JQuery" */
 
+const pageHeaderDiv = document.getElementById('currentPageHeader');
 const mainElement = document.querySelector('main');
+const homePage = document.createElement('h3');
+
+let currentPage = '';
+let raceData = '';
+
+function goHome() {
+    // Clear previous page
+    clearPrevPage();
+
+    // Set page header to HOME
+    currentPage = 'Home';
+    SetHeader(currentPage);
+
+    // Add HOME info to <main>
+    const homePageTxt = document.createTextNode('Welcome to homepage');
+    homePage.appendChild(homePageTxt);
+    mainElement.appendChild(homePage);
+}
+
+goHome()
 
 async function getRaces() {
- const raceIndexPromise = await fetch('https://www.dnd5eapi.co/api/races');
- const raceIndex = await raceIndexPromise.json();
 
- raceIndex.results.forEach(eachRace => {
+ /*    let raceIndex = ""
+
+    if (verifyLoadNeed(raceData) == true) {
+        console.log('true');
+    } else {
+        console.log('false');
+        const raceIndexPromise = await fetch('https://www.dnd5eapi.co/api/races');
+        raceIndex = await raceIndexPromise.json();
+        raceData = raceIndex.results;
+    }
+ */
+    const raceIndexPromise = await fetch('https://www.dnd5eapi.co/api/races');
+    raceIndex = await raceIndexPromise.json();
+    raceData = raceIndex.results;
+
+    // Set data to variable to prevent loading from API on next run:
+    currentPage = 'Races';
+
+    // Clear previous page and set new one:
+    clearPrevPage();
+    SetHeader(currentPage);
+
+    // Add cards
+    addCards(raceIndex.results);
+
+
+
+
+/*     raceIndex.results.forEach(eachRace => {
     createCard(eachRace);
-    });
+    }); */
 }
-getRaces();
+
 
 function createCard(data) {
+    // Clear previous page
+
+
     const card = document.createElement('article');
 
     const raceName = document.createElement('h3');
@@ -20,7 +70,6 @@ function createCard(data) {
     raceName.appendChild(raceNameTxt);
 
     const racePic = document.createElement('img');
-
     racePic.id = data.index
 
     const buttonContainer = document.createElement('div');
@@ -40,4 +89,70 @@ function createCard(data) {
 /*     const racePicIndex = data.index.toString()
     const racePicSrc = `./images/races/${racePicIndex}`
  */
-   }
+}
+
+function SetHeader (title) {
+    const curPageHeader = document.createElement('h2');
+    const pageHeader = document.getElementById('currentPageHeader');
+    const pageHeaderTxt = document.createTextNode(title);
+
+    curPageHeader.appendChild(pageHeaderTxt);
+    pageHeader.appendChild(curPageHeader);
+}
+
+
+homeNav = document.getElementById('#home');
+characterNav = document.getElementById('#character');
+monsterNav = document.getElementById('#moster');
+itemNav = document.getElementById('#item');
+miscNav = document.getElementById('#misc');
+
+allNav = document.querySelectorAll('nav ul li a');
+
+// console.log(allNav);
+
+
+function setNavListen() {
+    allNav.forEach( eachItem => {
+        eachItem.addEventListener('click', function(){
+            if (eachItem.id == 'Races') {
+                getRaces();
+            } else {
+                goHome();
+            }
+        })
+        console.log(eachItem.id);
+        });
+}
+setNavListen();
+
+/*//////////////////////////
+// Helper Functions:      //
+//////////////////////////*/
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// Use reremoveAllChildNodes() function to clear the page.
+function clearPrevPage() {
+    removeAllChildNodes(homePage);
+    removeAllChildNodes(pageHeaderDiv);
+    removeAllChildNodes(mainElement);
+}
+
+function verifyLoadNeed(asset) {
+    if (asset !== "") {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function addCards(array) {
+    array.forEach(eachItem => {
+    createCard(eachItem);
+    });
+}
--- D&D_API_5e/scriptTemp.js
+++ D&D_API_5e/scriptTemp.js
@@ -1,79 +1,158 @@
 /* Created on 10/11/23 by ArchILLtect for the Coursera program "Javascript for Beginners Specialization" - Course 2 - "Animation with JavaScript and JQuery" */
 
+const pageHeaderDiv = document.getElementById('currentPageHeader');
 const mainElement = document.querySelector('main');
+const homePage = document.createElement('h3');
 
-let movieData;
+let currentPage = '';
+let raceData = '';
 
-async function getMovies() {
-    const moviesPromise = await fetch('https://www.dnd5eapi.co/api/races/');
-    const allMovies = await moviesPromise.json();
+function goHome() {
+    // Clear previous page
+    clearPrevPage();
 
-    setSort(allMovies.movies);
-    addCards(allMovies.movies);
-    movieData = allMovies;
+    // Set page header to HOME
+    currentPage = 'Home';
+    SetHeader(currentPage);
 
-/*     allMovies.movies.forEach(eachMovie => {
-        createCard(eachMovie);
-    });
+    // Add HOME info to <main>
+    const homePageTxt = document.createTextNode('Welcome to homepage');
+    homePage.appendChild(homePageTxt);
+    mainElement.appendChild(homePage);
+}
+
+goHome()
+
+async function getRaces() {
+
+ /*    let raceIndex = ""
+
+    if (verifyLoadNeed(raceData) == true) {
+        console.log('true');
+    } else {
+        console.log('false');
+        const raceIndexPromise = await fetch('https://www.dnd5eapi.co/api/races');
+        raceIndex = await raceIndexPromise.json();
+        raceData = raceIndex.results;
+    }
+ */
+    const raceIndexPromise = await fetch('https://www.dnd5eapi.co/api/races');
+    raceIndex = await raceIndexPromise.json();
+    raceData = raceIndex.results;
+
+    // Set data to variable to prevent loading from API on next run:
+    currentPage = 'Races';
+
+    // Clear previous page and set new one:
+    clearPrevPage();
+    SetHeader(currentPage);
+
+    // Add cards
+    addCards(raceIndex.results);
+
+
+
+
+/*     raceIndex.results.forEach(eachRace => {
+    createCard(eachRace);
+    }); */
+}
+
+
+function createCard(data) {
+    // Clear previous page
+
+
+    const card = document.createElement('article');
+
+    const raceName = document.createElement('h3');
+    const raceNameTxt = document.createTextNode(data.name);
+    raceName.appendChild(raceNameTxt);
+
+    const racePic = document.createElement('img');
+    racePic.id = data.index
+
+    const buttonContainer = document.createElement('div');
+    const selectButton = document.createElement('button');
+    const selectButtonTxt = document.createTextNode("Click for more info");
+    selectButton.appendChild(selectButtonTxt);
+
+    card.appendChild(raceName);
+    card.appendChild(racePic);
+    card.appendChild(buttonContainer);   
+    buttonContainer.appendChild(selectButton);
+    mainElement.appendChild(card);
+
+    const raceImg = document.querySelector(`#${data.index}`)
+
+    raceImg.src = `./images/races/${data.index}.jpg`;
+/*     const racePicIndex = data.index.toString()
+    const racePicSrc = `./images/races/${racePicIndex}`
  */
 }
 
-getMovies();
+function SetHeader (title) {
+    const curPageHeader = document.createElement('h2');
+    const pageHeader = document.getElementById('currentPageHeader');
+    const pageHeaderTxt = document.createTextNode(title);
 
-function createCard(data) {
-    const card = document.createElement('article');
-
-    const movieTitle = document.createElement('h2');
-    const movieTitleTxt = document.createTextNode(data.title);
-    movieTitle.appendChild(movieTitleTxt);
-
-    const director = document.createElement('p');
-    const directorTxt = document.createTextNode(`Director: ${data.mainDirector.name}`);
-    director.appendChild(directorTxt);
-
-    //write similar code for year, description and score
-    const year = document.createElement('p');
-    const yearTxt = document.createTextNode(`Released: ${data.usReleaseDate}`);
-    year.appendChild(yearTxt);
-
-    const stardateStart = document.createElement('p');
-    const stardateStartTxt = document.createTextNode(`Star Date From: ${data.stardateFrom}`);
-    stardateStart.appendChild(stardateStartTxt);
-
-    const stardateEnd = document.createElement('p');
-    const stardateEndTxt = document.createTextNode(`Star Date To: ${data.stardateTo}`);
-    stardateEnd.appendChild(stardateEndTxt);
-
-    card.appendChild(movieTitle);
-    card.appendChild(director);
-    // append the year, description and score
-    card.appendChild(year);
-    card.appendChild(stardateStart);
-    card.appendChild(stardateEnd);
-
-    mainElement.appendChild(card);
-
-    document.getElementById('sortorder').removeAttribute('disabled');
+    curPageHeader.appendChild(pageHeaderTxt);
+    pageHeader.appendChild(curPageHeader);
 }
 
-document.getElementById('sortorder').addEventListener('change', function () {
-    mainElement.innerHTML = '';
-    setSort(movieData.movies);
-    addCards(movieData.movies);
-   });
 
-function setSort(array) {
-    const sortOrder = document.getElementById('sortorder').value;
-    switch (sortOrder) {
-        case 'title': array.sort((a, b) => (a.title > b.title) ? 1 : -1); break;
-        case 'mainDirector': array.sort((a, b) => (a.mainDirector.name > b.mainDirector.name) ? 1 : -1); break;
-        case 'usReleaseDate': array.sort((a, b) => (a.usReleaseDate > b.usReleaseDate) ? 1 : -1); break;
-        case 'stardateFrom': array.sort((a, b) => (a.stardateFrom >b.stardateFrom) ? 1 : -1); break;
+homeNav = document.getElementById('#home');
+characterNav = document.getElementById('#character');
+monsterNav = document.getElementById('#moster');
+itemNav = document.getElementById('#item');
+miscNav = document.getElementById('#misc');
+
+allNav = document.querySelectorAll('nav ul li a');
+
+// console.log(allNav);
+
+
+function setNavListen() {
+    allNav.forEach( eachItem => {
+        eachItem.addEventListener('click', function(){
+            if (eachItem.id == 'Races') {
+                getRaces();
+            } else {
+                goHome();
+            }
+        })
+        console.log(eachItem.id);
+        });
+}
+setNavListen();
+
+/*//////////////////////////
+// Helper Functions:      //
+//////////////////////////*/
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// Use reremoveAllChildNodes() function to clear the page.
+function clearPrevPage() {
+    removeAllChildNodes(homePage);
+    removeAllChildNodes(pageHeaderDiv);
+    removeAllChildNodes(mainElement);
+}
+
+function verifyLoadNeed(asset) {
+    if (asset !== "") {
+        return false;
+    } else {
+        return true;
     }
 }
 
 function addCards(array) {
     array.forEach(eachItem => {
-        createCard(eachItem);
+    createCard(eachItem);
     });
-   }
+}
